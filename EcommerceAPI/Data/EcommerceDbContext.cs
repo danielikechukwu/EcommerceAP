217a04322dab0c0ae1b116eb36ee44d75604d160
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-04-19 15:01:27 +0100

    feat(DbContext): Register DbContext

--- EcommerceAPI/Data/EcommerceDbContext.cs
+++ EcommerceAPI/Data/EcommerceDbContext.cs
@@ -119,5 +119,15 @@ namespace EcommerceAPI.Data
                 }
                 );
         }
+
+        public DbSet<Customer> Customer { get; set; }
+
+        public DbSet<Order> Order { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+
     }
 }
--- EcommerceAPI/Program.cs
+++ EcommerceAPI/Program.cs
@@ -1,11 +1,21 @@
+using EcommerceAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Configure EF Core with SQL Server
+builder.Services.AddDbContext<EcommerceDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EFCoreDBConnection")));
 
 var app = builder.Build();
 
