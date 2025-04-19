9c7b367286319aa95efc681cecaaa0fa108ce384
Daniel ikechukwu <daniel.ikechukwu@avosoft.co.uk>
2025-04-19 17:51:20 +0100

    refact(controller): Implement a controller for customers.

---
+++ EcommerceAPI/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using EcommerceAPI.Data;
+using EcommerceAPI.DTOs;
+using EcommerceAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly EcommerceDbContext _context;
+
+        public CustomerController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        //Register new customer
+        //Demostrate FromForm
+        //Endpoint: POST /api/customers/register
+        //[HttpPost("register")]
+        //public async Task<ActionResult<Customer>> RegisterCustomer([FromForm] CustomerRegistrationDTO registerCustomer)
+        //{
+
+        //}
+
+    }
+}
