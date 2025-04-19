cc57fef07bfbbfc8d2b6e460aac0189d60458bec
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-04-19 12:09:32 +0100

    feat(model): Implement new models showing relationship between them.

---
+++ EcommerceAPI/Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace EcommerceAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        //Relation: One customer can have many order
+        [Required]
+        public int CustomerId { get; set; }
+
+        [JsonIgnore]
+        public Customer Customer { get; set; }
+
+        [Required]
+        public string OrderStatus { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal OrderAmount { get; set; }
+
+        // One Order can have multiple OrderItems
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
